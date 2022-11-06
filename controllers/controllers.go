d38d44fe4794957f7a450daa8321ee7af6842738
rakshith <rakshit.s.gowda@gmail.com>
2022-11-06 20:47:30 +0530

    models and few controller functions blueprint

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,26 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+// basic controllers for user
+// SignUp
+// Login
+// password hash and helpers
+
+func HashPassword(password string) string {
+
+}
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc    {}
+func SearchProduct() gin.HandlerFunc         {}
+func SearchProductsByQuery() gin.HandlerFunc {}
--- main.go
+++ main.go
@@ -7,27 +7,35 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rakshithsgowda/e-commerce-golang-practise/controllers"
 	"github.com/rakshithsgowda/e-commerce-golang-practise/database"
+	"github.com/rakshithsgowda/e-commerce-golang-practise/middlewares"
 	"github.com/rakshithsgowda/e-commerce-golang-practise/routes"
 )
 
-
-func main()  {
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
-	app := controllers.NewApplication(database.ProductData(database.Client,"Products"), database.UserData(database.Client,"Users"))
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
 
 	routes.UserRoutes(router)
-	router.Use(middleware.Authentication())
+	router.Use(middlewares.Authentication())
 
-	router.GET("/addtocart",app.Addtocart())
-	router.GET("/removeitem",app.RemoveItem())
-	router.GET("/cartcheckout",app.BuyFromCart())
-	router.GET("/instantbuy",app.InstantBuy())
+	// direct homepage router actions
+	router.GET("/addtocart", app.AddTocart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
 
-	log.Fatal(router.Run(":"+port))
+	// after buy non authenticated user address input(CRUD)
+
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddress", controllers.DeleteAddress())
+	router.GET("/listcart", controllers.GetItemsFromCart())
+	log.Fatal(router.Run(":" + port))
 }
