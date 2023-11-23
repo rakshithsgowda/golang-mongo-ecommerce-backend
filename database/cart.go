20a3880b3154b729b891bb5df86572552f8b45f0
rakshith <rakshit.s.gowda@gmail.com>
2023-11-23 23:57:16 +0530

    add product to cart-db

--- database/cart.go
+++ database/cart.go
@@ -3,23 +3,53 @@ package database
 import (
 	"context"
 	"errors"
+	"log"
 
+	"github.com/rakshithsgowda/golang-mongo-ecommerce-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
-	ErrCantFindProduct     = errors.New("can't find the product")
-	ErrCantDecodeProdducts = errors.New("cant find the product")
-	ErrUserIdIsNotValid    = errors.New("this user is not valid")
-	ErrCantUpdateUser      = errors.New("cannot add this product to the cart")
-	ErrCantRemoveItemCart  = errors.New("cant remove item from cart")
-	ErrCantGetItem         = errors.New("cannot get item from cart")
-	ErrCantBuyCartItem     = errors.New("cannot update the purchase")
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProducts = errors.New("cant find the product")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("cannot add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("cant remove item from cart")
+	ErrCantGetItem        = errors.New("cannot get item from cart")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
 )
 
-func AddProductToCart(ctx context.Context, prodCollection *mongo.Collection, userCollection *mongo.Collection, productID primitive.ObjectID, userQueryID string) {
+func AddProductToCart(ctx context.Context, prodCollection *mongo.Collection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+
+	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productcart []models.ProductUser
+	err = searchfromdb.All(ctx, &productcart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productcart}}}}}}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 }
+
 func RemoveCartItem(ctx context.Context, prodCollection *mongo.Collection, userCollection *mongo.Collection, productID primitive.ObjectID, userQueryID string) {
 }
 func BuyItemFromCart() {}
